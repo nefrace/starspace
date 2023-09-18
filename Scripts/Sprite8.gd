5ac8a334de33f6c5cdf8f29bc3597a1a58544f43
nefrace <nefrace@gmail.com>
2023-09-19 01:14:32 +0300

    Added hint to angle for Sprite8

--- Scripts/Sprite8.gd
+++ Scripts/Sprite8.gd
@@ -3,7 +3,7 @@ extends Sprite2D
 class_name Sprite8
 
 @export var is_repeating := false
-@export var angle: float = 0
+@export_range(0, 360) var angle: float = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
